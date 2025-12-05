6c97a30bc763a22e7fedc410685af59a0bf01fe7
Joseph Ode Ona <josephfaradfreelance@gmail.com>
2025-12-05 12:08:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,14 @@ import streamlit as st
 
 st.title('Farad Mini POS Machine')
 
-st.info("Welcome to Farad's POS Menu")
+st.write("Welcome to Farad's POS Menu")
+# --- Initialize balance in session state ---
+if "balance" not in st.session_state:
+    st.session_state.balance = 1000
+
+st.subheader("Please Select Option to Continue")
+
+option = st.radio(
+    "Choose an action:",
+    ["Check Balance", "Deposit Money", "Withdraw Money", "Exit"]
+)
